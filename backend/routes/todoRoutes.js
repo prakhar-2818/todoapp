1a9024b958c760d7f1d964ce98a16f5ab94cbb3e
Prakhar <prakharshakya28@gmail.com>
2026-05-13 06:16:13 +0530

    Replace todo routes with a test route
    Removed all existing todo routes and added a test route.

--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -1,63 +1,10 @@
 const express = require("express");
-
 const router = express.Router();
 
-const Todo = require("../models/Todo");
-
-
-
-router.post("/addTodo", async (req, res) => {
-
-    const todo = new Todo({
-
-        title: req.body.title
-
-    });
-
-    await todo.save();
-
-    res.send("Todo Added");
-
+router.get("/", (req, res) => {
+    res.json([
+        { task: "Test Task" }
+    ]);
 });
-
-
-
-router.get("/getTodos", async (req, res) => {
-
-    const todos = await Todo.find();
-
-    res.json(todos);
-
-});
-
-
-
-router.delete("/deleteTodo/:id", async (req, res) => {
-
-    await Todo.findByIdAndDelete(req.params.id);
-
-    res.send("Todo Deleted");
-
-});
-
-
-
-router.put("/updateTodo/:id", async (req, res) => {
-
-    await Todo.findByIdAndUpdate(
-
-        req.params.id,
-
-        {
-            completed: req.body.completed
-        }
-
-    );
-
-    res.send("Todo Updated");
-
-});
-
-
 
 module.exports = router;
